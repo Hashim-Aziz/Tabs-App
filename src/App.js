2d6e94b0a4aa2755a2148f13e5bff787701522ff
HashimAziz <hashimkayani97@gmail.com>
2021-03-30 08:41:05 +0500

    project setup completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tabs-project";
+
 function App() {
   return <h1>Hello world</h1>;
 }
